89e7a694e8228c88d175c878db7145e897735458
Alexander V. Nikolaev <avn@daemon.hole.ru>
2010-02-03 17:45:02 +0000

    Use HELP_WINDOW_ID definition insted direct reference to property name

--- src/eoi_help.c
+++ src/eoi_help.c
@@ -41,6 +41,8 @@
 
 #define THEME_EDJ (THEME_DIR "/help.edj")
 
+#define HELP_WINDOW_ID "libeoi-help-window"
+
 typedef struct {
     Evas_Object *textbox;
     char *application;
@@ -260,7 +262,7 @@ _default_page_updated_handler(Evas_Object * help,
                               void *param __attribute__ ((unused)))
 {
     Evas *evas = evas_object_evas_get(help);
-    Evas_Object *helpwin = evas_object_name_find(evas, "help-window");
+    Evas_Object *helpwin = evas_object_name_find(evas, HELP_WINDOW_ID);
     choicebox_aux_edje_footer_handler(helpwin, "footer", cur_page,
                                       total_pages);
 }
@@ -269,7 +271,7 @@ static void
 _default_help_closed(Evas_Object * help)
 {
     Evas *evas = evas_object_evas_get(help);
-    Evas_Object *helpwin = evas_object_name_find(evas, "help-window");
+    Evas_Object *helpwin = evas_object_name_find(evas, HELP_WINDOW_ID);
     Evas_Object *focus = evas_object_data_get(helpwin, "prev-focus");
     evas_object_hide(helpwin);
     evas_object_del(helpwin);
@@ -287,7 +289,7 @@ eoi_help_show(Evas * canvas,
     Evas_Object *helpwin = eoi_main_window_create(canvas);
     edje_object_part_text_set(helpwin, "title", help_win_title);
     edje_object_part_text_set(helpwin, "footer", "0/0");
-    evas_object_name_set(helpwin, "help-window");
+    evas_object_name_set(helpwin, HELP_WINDOW_ID);
     evas_object_move(helpwin, 0, 0);
     int w, h;
     evas_output_size_get(canvas, &w, &h);
