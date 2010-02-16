e54c7da613954c851c487e24a2dc4afd20aaeee5
Mikhail Gusarov <dottedmag@dottedmag.net>
2010-02-17 00:28:38 +0100

    Update copyright years

--- src/libeoi.c
+++ src/libeoi.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2009 Mikhail Gusarov <dottedmag@dottedmag.net>
+ * Copyright © 2009,2010 Mikhail Gusarov <dottedmag@dottedmag.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
